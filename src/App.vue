7c18bf37fef1863c7ab8c7d716cd28ca99f98cc1
shimbuichi <contact.shimbuichi@gmail.com>
2019-03-27 07:38:37 +0900

    vuetifyテンプレート適用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
-<template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-app id="inspire">
+        <v-navigation-drawer
+                v-model="drawer"
+                :clipped="$vuetify.breakpoint.lgAndUp"
+                fixed
+                app
+        >
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-layout
+                            v-if="item.heading"
+                            :key="item.heading"
+                            row
+                            align-center
+                    >
+                        <v-flex xs6>
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-flex>
+                        <v-flex xs6 class="text-xs-center">
+                            <a href="#!" class="body-2 black--text">EDIT</a>
+                        </v-flex>
+                    </v-layout>
+                    <v-list-group
+                            v-else-if="item.children"
+                            :key="item.text"
+                            v-model="item.model"
+                            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                            append-icon=""
+                    >
+                        <template v-slot:activator>
+                            <v-list-tile>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>
+                                        {{ item.text }}
+                                    </v-list-tile-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </template>
+                        <v-list-tile
+                                v-for="(child, i) in item.children"
+                                :key="i"
+                                @click=""
+                        >
+                            <v-list-tile-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ child.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list-group>
+                    <v-list-tile v-else :key="item.text" @click="">
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>
+                                {{ item.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar
+                :clipped-left="$vuetify.breakpoint.lgAndUp"
+                color="blue darken-3"
+                dark
+                app
+                fixed
+        >
+            <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                <span class="hidden-sm-and-down">Dassai</span>
+            </v-toolbar-title>
+            <v-text-field
+                    flat
+                    solo-inverted
+                    hide-details
+                    prepend-inner-icon="search"
+                    label="Search"
+                    class="hidden-sm-and-down"
+            ></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>apps</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>notifications</v-icon>
+            </v-btn>
+            <v-btn icon large>
+                <v-avatar size="32px" tile>
+                    <img
+                            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+                            alt="Vuetify"
+                    >
+                </v-avatar>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <router-view/>
+        </v-content>
+    </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
@@ -30,5 +117,39 @@ import HelloWorld from './components/HelloWorld.vue';
         HelloWorld,
     },
 })
-export default class extends Vue {}
+export default class App extends Vue {
+    public drawer: boolean = false;
+    public items: any[] = [
+        { icon: 'contacts', text: 'Contacts' },
+        { icon: 'history', text: 'Frequently contacted' },
+        { icon: 'content_copy', text: 'Duplicates' },
+        {
+            'icon': 'keyboard_arrow_up',
+            'icon-alt': 'keyboard_arrow_down',
+            'text': 'Labels',
+            'model': true,
+            'children': [
+                { icon: 'add', text: 'Create label' },
+            ],
+        },
+        {
+            'icon': 'keyboard_arrow_up',
+            'icon-alt': 'keyboard_arrow_down',
+            'text': 'More',
+            'model': false,
+            'children': [
+                { text: 'Import' },
+                { text: 'Export' },
+                { text: 'Print' },
+                { text: 'Undo changes' },
+                { text: 'Other contacts' },
+            ],
+        },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Send feedback' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Go to the old version' },
+    ];
+}
 </script>
