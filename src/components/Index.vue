077fb0001a2a329da09fa3ec84f3a78748083aae
masami.shimbuichi <kei11nishikori16@gmail.com>
2019-04-04 14:37:55 +0900

    リストのコンポーネント化、酒評価用のカード追加

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,40 +1,21 @@
 <template>
-    <v-layout>
-        <v-flex>
-            <v-card>
-                <v-toolbar color="cyan" dark>
-                    <v-toolbar-side-icon>
-                        <v-toolbar-title>
-                            Inbox
-                        </v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-btn icon>
-                            <v-icon>search</v-icon>
-                        </v-btn>
-                    </v-toolbar-side-icon>
-                </v-toolbar>
-            </v-card>
-            <v-list two-line>
-                <template>
-                    <v-subheader>aaaa</v-subheader>
-                    <v-divider>bbbb</v-divider>
-                    <v-list-tile>
-                        <v-list-tile-avatar>
-                            <img :src="'https://cdn.vuetifyjs.com/images/lists/1.jpg'">
-                        </v-list-tile-avatar>
-                        <v-list-tile-content>
-                            <v-list-tile-title v-html="'hoge'"></v-list-tile-title>
-                            <v-list-tile-sub-title v-html="'sub'"></v-list-tile-sub-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-            </v-list>
+    <v-layout row wrap>
+        <v-flex xs6>
+            <ds-list-item/>
+        </v-flex>
+        <v-flex xs6>
+            <ds-list-item/>
+        </v-flex>
+        <v-flex xs12>
+            <ds-sake-card/>
         </v-flex>
     </v-layout>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator';
+  import DsSakeCard from '@/components/molecules/DsSakeCard.vue';
+  import DsListItem from '@/components/molecules/DsListItem.vue';
   import {
     VBtn,
     VCard,
@@ -44,6 +25,7 @@
     VList,
     VListTile,
     VListTileAvatar,
+    VListTileContent,
     VListTileSubTitle,
     VListTileTitle,
     VSpacer,
@@ -54,13 +36,14 @@
   } from 'vuetify/lib';
 
   @Component({
-  components: {
-    VLayout, VFlex, VCard, VToolbar, VToolbarSideIcon, VToolbarTitle, VSpacer, VBtn,
-    VListTileTitle, VListTileSubTitle, VListTile, VDivider, VSubheader, VList, VListTileAvatar,
-  },
-})
-export default class Index extends Vue {
-}
+    components: {
+      DsSakeCard, DsListItem,
+      VLayout, VFlex, VCard, VToolbar, VToolbarSideIcon, VToolbarTitle, VSpacer, VBtn,
+      VListTileTitle, VListTileContent, VListTileSubTitle, VListTile, VDivider, VSubheader, VList, VListTileAvatar,
+    },
+  })
+  export default class Index extends Vue {
+  }
 </script>
 
 <style scoped>
---
+++ src/components/molecules/DsListItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-list two-line>
+        <v-subheader>{{ subHeader }}</v-subheader>
+        <v-divider>{{ divider }}</v-divider>
+        <v-list-tile>
+            <v-list-tile-avatar>
+                <img :src="avatarSrc">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+                <v-list-tile-title v-html="listTitle"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="listSubTitle"></v-list-tile-sub-title>
+            </v-list-tile-content>
+        </v-list-tile>
+    </v-list>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import {
+  VDivider,
+  VList,
+  VListTile,
+  VListTileAvatar,
+  VListTileContent,
+  VListTileSubTitle,
+  VListTileTitle,
+  VSubheader,
+} from 'vuetify/lib';
+
+@Component({
+  components: {
+    VListTile, VListTileContent, VListTileSubTitle, VListTileAvatar, VSubheader, VDivider,
+    VListTileTitle, VList,
+  },
+})
+export default class DsListItem extends Vue {
+  public subHeader: string = 'aaaa';
+  public divider: string = 'bbbb';
+  public avatarSrc: string = 'https://cdn.vuetifyjs.com/images/lists/1.jpg';
+  public listTitle: string = 'hoge';
+  public listSubTitle: string = 'fuga';
+}
+</script>
+
+<style scoped>
+</style>
